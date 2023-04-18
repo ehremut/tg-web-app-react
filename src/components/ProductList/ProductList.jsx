e34c4e696501a11ef81039e96e753cb3ea498ebf
Andarbek Akhmadov <andarbek_akhmadov@senla.eu>
2023-04-19 00:13:15 +0400

    new commit

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -15,24 +15,24 @@ const products = [
 ]
 
 const getTotalPrice = (items = []) => {
-    return item.reduce((acc, item) => {
-        return acc += item.price
+    return items.reduce((acc, item) => {
+        return acc + item.price
     }, 0)
 }
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([])
     const {tg} = useTelegram();
     const onAdd = (product) => {
-        const alreadyAdded = addedItems. find(item => item.id === product.id);
-        let newItems = [];
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems;
         if(alreadyAdded) {
-            newItems = addedItems.filter (item => item. id !== product.id);
+            newItems = addedItems.filter(item => item. id !== product.id);
         } else {
             newItems = [...addedItems, product];
         }
         setAddedItems(newItems)
 
-        if (newItems.length.isEmpty) {
+        if (newItems.length === 0) {
             tg.MainButton.hide()
         } else {
             tg.MainButton.show()
